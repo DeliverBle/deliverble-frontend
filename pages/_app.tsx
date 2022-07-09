80a4db2386f8527ad055ba09419d555d4506f1d9
100Gyeon <bjy9966@naver.com>
2022-07-09 14:41:36 +0900

    init: React Query 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
 
