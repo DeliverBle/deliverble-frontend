314e0ae6454350de509dbce04e0234e7cfea8f43
100Gyeon <bjy9966@naver.com>
2022-07-09 14:36:44 +0900

    init: Recoil 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
 
 export default MyApp;
