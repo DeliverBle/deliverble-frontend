db9d35afbff7b7932be7c8442e53538b615cef19
100Gyeon <bjy9966@naver.com>
2022-07-11 17:57:00 +0900

    feat: lottie 라이브러리, 예시 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
 import Head from 'next/head';
+import Lottie from 'lottie-react';
+import { lottie } from 'public/assets/lottie';
+import styled from 'styled-components';
 
-function Home() {
+function Landing() {
   return (
     <div>
       <Head>
         <title>DeliverBle</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Home</div>
+      <div>Landing</div>
+      <StLottieWrapper>
+        <Lottie animationData={lottie} autoPlay loop />
+      </StLottieWrapper>
     </div>
   );
 }
 
-export default Home;
+export default Landing;
+
+const StLottieWrapper = styled.div`
+  width: 50rem;
+  height: 50rem;
+`;
