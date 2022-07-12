f65391a3fa2bead5bb5ff1886bba83f1c4dbb8fc
100Gyeon <bjy9966@naver.com>
2022-07-13 05:30:27 +0900

    feat: 영상 검색 조건 선택 기능 구현 완료

--- src/components/SelectBox.tsx
+++ src/components/SelectBox.tsx
@@ -19,22 +19,19 @@ function SelectBox(props: SelectBoxProps) {
     checkedList.includes(checkedItem)
       ? setCheckedList(checkedList.filter((item) => item !== checkedItem))
       : setCheckedList([...checkedList, checkedItem]);
+
+    if (checkedItem === '전체') setCheckedList(['전체']);
   };
 
-  // 모두 해제되면 전체 선택
-  if (!checkedList.length) {
-    setCheckedList(['전체']);
-  }
+  // 조건 모두 해제되면 '전체' 선택
+  if (!checkedList.length) setCheckedList(['전체']);
 
-  // 조건을 하나라도 선택하면 전체 부분에 있는 체크는 해제되어야 함
-  if (checkedList.includes('전체') && checkedList.length >= 2 && checkedList.length < optionList.length) {
+  // 조건을 하나라도 선택하면 '전체' 부분에 있는 체크는 해제되어야 함
+  if (checkedList.includes('전체') && checkedList.length >= 2 && checkedList.length < optionList.length)
     setCheckedList(checkedList.filter((item) => item !== '전체'));
-  }
 
-  // 6번 요구사항 : 다시 전체를 선택하면 전체에만 체크
-  if (checkedList.includes('전체') && checkedList.length >= optionList.length) {
-    setCheckedList(['전체']);
-  }
+  // 다시 '전체'를 선택하면 '전체'에만 체크되도록
+  if (checkedList.includes('전체') && checkedList.length >= optionList.length) setCheckedList(['전체']);
 
   return (
     <StSelectBox isClicked={isClicked}>
