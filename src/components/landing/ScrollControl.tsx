d37bff3cfd37e671a89d24d6be289a06c614fba9
songah <skutcm119@gmail.com>
2022-07-13 06:56:16 +0900

    refactor: css 코드 리팩토링

--- src/components/landing/ScrollControl.tsx
+++ src/components/landing/ScrollControl.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-
+import { useEffect } from 'react';
 interface ScrollContainerProps {
   slideNumber: number;
   setSlideNumber: (slideNumber: number) => void;
@@ -8,56 +8,46 @@ interface ScrollContainerProps {
 function ScrollContainer(props: ScrollContainerProps) {
   const { slideNumber, setSlideNumber } = props;
 
+  useEffect(() => {
+    console.log(slideNumber);
+  }, [slideNumber]);
+
   return (
-    <StScrollContainer slideNumber={slideNumber}>
-      <li className="first-slide" onClick={() => setSlideNumber(1)}></li>
-      <li className="second-slide" onClick={() => setSlideNumber(2)}></li>
-      <li className="third-slide" onClick={() => setSlideNumber(3)}></li>
-      <li className="fourth-slide" onClick={() => setSlideNumber(4)}></li>
-      <li className="fifth-slide" onClick={() => setSlideNumber(5)}></li>
-      <li className="sixth-slide" onClick={() => setSlideNumber(6)}></li>
+    <StScrollContainer>
+      <li className={slideNumber === 1 ? 'isActive' : 'unActive'} onClick={() => setSlideNumber(1)}></li>
+      <li className={slideNumber === 2 ? 'isActive' : 'unActive'} onClick={() => setSlideNumber(2)}></li>
+      <li className={slideNumber === 3 ? 'isActive' : 'unActive'} onClick={() => setSlideNumber(3)}></li>
+      <li className={slideNumber === 4 ? 'isActive' : 'unActive'} onClick={() => setSlideNumber(4)}></li>
+      <li className={slideNumber === 5 ? 'isActive' : 'unActive'} onClick={() => setSlideNumber(5)}></li>
+      <li className={slideNumber === 6 ? 'isActive' : 'unActive'} onClick={() => setSlideNumber(6)}></li>
     </StScrollContainer>
   );
 }
 
 export default ScrollContainer;
 
-const StScrollContainer = styled.ul<{ slideNumber: number }>`
+const StScrollContainer = styled.ul`
   position: fixed;
   margin-top: 42.4rem;
   right: 6.4rem;
 
   z-index: 100;
 
+  & > .isActive {
+    background-color: blue;
+  }
+
+  & > .unActive {
+    background-color: lightgray;
+  }
+
   & > li {
     width: 2.2rem;
     height: 2.2rem;
     border-radius: 50%;
     margin: 0 0 1.1111111111111112rem;
-    opacity: 0.5;
     cursor: pointer;
     margin-bottom: 2rem;
-  }
-  .first-slide {
-    background-color: ${({ slideNumber }) => (slideNumber === 1 ? 'blue' : 'lightgray')};
-  }
-
-  .second-slide {
-    background-color: ${({ slideNumber }) => (slideNumber === 2 ? 'blue' : 'lightgray')};
-  }
-
-  .third-slide {
-    background-color: ${({ slideNumber }) => (slideNumber === 3 ? 'blue' : 'lightgray')};
-  }
-
-  .fourth-slide {
-    background-color: ${({ slideNumber }) => (slideNumber === 4 ? 'blue' : 'lightgray')};
-  }
-
-  .fifth-slide {
-    background-color: ${({ slideNumber }) => (slideNumber === 5 ? 'blue' : 'lightgray')};
-  }
-  .sixth-slide {
-    background-color: ${({ slideNumber }) => (slideNumber === 6 ? 'blue' : 'lightgray')};
+    opacity: 0.5;
   }
 `;
