f8db9c2fa1e6c3388dd80e9dd855043b92759033
100Gyeon <bjy9966@naver.com>
2022-07-13 00:04:53 +0900

    asset: 학습 기능 설명 모달을 위한 에셋 추가

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -1,3 +1,8 @@
 export { default as icArrow } from './ic_arrow.svg';
 export { default as icDelete } from './ic_delete.svg';
 export { default as icSearch } from './ic_search.svg';
+export { default as icCloseButton } from './ic_close_button.svg';
+export { default as icGuide } from './ic_guide.svg';
+export { default as icGuideHighlight } from './ic_guide_highlight.svg';
+export { default as icGuideSpacing } from './ic_guide_spacing.svg';
+export { default as icXButton } from './ic_x_button.svg';
