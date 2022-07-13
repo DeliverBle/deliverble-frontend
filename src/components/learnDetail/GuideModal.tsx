fe53ae92da2092f24898d9bcbcde2262a920b442
100Gyeon <bjy9966@naver.com>
2022-07-13 19:25:24 +0900

    chore: 모달창 위치 수정

--- src/components/learnDetail/GuideModal.tsx
+++ src/components/learnDetail/GuideModal.tsx
@@ -83,9 +83,9 @@ const StGuideModalContent = styled.div`
   box-shadow: 0.4rem 0.4rem 2rem rgba(22, 15, 53, 0.15);
 
   position: fixed;
-  top: 50%;
+  top: 54.7rem;
   left: 50%;
-  transform: translate(-50%, -50%);
+  transform: translateX(-50%);
   z-index: 2;
 
   & > div:first-child {
