671aa400a8ea926245f6beda5f754172d34ce00b
HyeJun <solar3070@naver.com>
2022-07-14 16:06:42 +0900

    asset: 좋아요 아이콘 변경

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -2,3 +2,6 @@ export { default as icArrow } from './ic_arrow.svg';
 export { default as icDelete } from './ic_delete.svg';
 export { default as icSearch } from './ic_search.svg';
 export { default as icScript } from './ic_script.svg';
+export { default as icLikeClicked } from './ic_like_clicked.svg';
+export { default as icLikeDefault } from './ic_like_default.svg';
+export { default as icLikeHover } from './ic_like_hover.svg';
