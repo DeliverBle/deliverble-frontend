e2cfb307b5918ac21181aab66014f7832e180601
100Gyeon <bjy9966@naver.com>
2022-07-15 03:44:23 +0900

    feat: mock data 사용해서 뉴스 정보 나타내기

--- src/pages/learn/[id].tsx
+++ src/pages/learn/[id].tsx
@@ -5,15 +5,31 @@ import ImageDiv from '../../components/common/ImageDiv';
 import GuideModal from '@src/components/learnDetail/GuideModal';
 import { icXButton, icGuide } from 'public/assets/icons';
 import { COLOR } from '@src/styles/color';
+import { FONT_STYLES } from '@src/styles/fontStyle';
+import { GetServerSidePropsContext } from 'next';
+import { api } from '@src/services/api';
+import { VideoData } from '@src/services/api/types/learn-detail';
 
-function LearnDetail() {
+function LearnDetail({ videoData }: { videoData: VideoData }) {
   const router = useRouter();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { title, channel, category, date, tagList } = videoData;
 
   return (
     <StLearnDetail>
       <ImageDiv onClick={() => router.back()} src={icXButton} className="close" layout="fill" alt="x" />
       <StLearnSection>
+        <StVideoDetail>
+          <div>
+            {channel} | {category} | {date}
+          </div>
+          <h1>{title}</h1>
+          <StTagContainer>
+            {tagList.map(({ tagId, tagName }) => (
+              <span key={tagId}>{tagName}</span>
+            ))}
+          </StTagContainer>
+        </StVideoDetail>
         <ImageDiv onClick={() => setIsModalOpen(true)} src={icGuide} className="guide" layout="fill" alt="?" />
       </StLearnSection>
       {isModalOpen && <GuideModal closeModal={() => setIsModalOpen(false)} />}
@@ -22,6 +38,12 @@ function LearnDetail() {
 }
 
 export default LearnDetail;
+
+export async function getServerSideProps({ params }: GetServerSidePropsContext) {
+  const id = +(params?.id ?? -1);
+  const response = await api.learnDetailService.getVideoData(id);
+  return { props: { videoData: response } };
+}
 
 const StLearnDetail = styled.div`
   padding: 16rem 10rem;
@@ -51,4 +73,33 @@ const StLearnSection = styled.section`
   height: 111.9rem;
   border-radius: 3rem;
   background-color: ${COLOR.WHITE};
+`;
+
+const StVideoDetail = styled.div`
+  & > div {
+    display: flex;
+    color: ${COLOR.GRAY_30};
+    ${FONT_STYLES.M_21_BODY};
+  }
+
+  & > h1 {
+    margin-top: 1.2rem;
+    margin-bottom: 2rem;
+    color: ${COLOR.BLACK};
+    ${FONT_STYLES.SB_32_HEADLINE};
+  }
+`;
+
+const StTagContainer = styled.div`
+  display: flex;
+  gap: 0.8rem;
+  margin-bottom: 4.8rem;
+
+  & > span {
+    padding: 1rem 1.6rem;
+    border-radius: 2.4rem;
+    color: ${COLOR.WHITE};
+    background-color: ${COLOR.MAIN_BLUE};
+    ${FONT_STYLES.SB_18_CAPTION};
+  }
 `;
