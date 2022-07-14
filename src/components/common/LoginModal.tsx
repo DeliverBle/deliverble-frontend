8793eab0bb6f18717f6f55cf29e6e8be4da6dbc6
choichoijin <cuj10594@yonsei.ac.kr>
2022-07-15 03:51:06 +0900

    bug: 스크롤시 반투명 배경 유지되도록 조정

--- src/components/common/LoginModal.tsx
+++ src/components/common/LoginModal.tsx
@@ -60,9 +60,11 @@ const StLoginModal = styled.div`
 
 const StLoginModalBackground = styled.div`
   position: absolute;
+  position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.78);
