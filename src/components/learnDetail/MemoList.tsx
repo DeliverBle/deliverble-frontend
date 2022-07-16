d154d8375678b7e8f117cacef34e7cfa0ce10903
HyeJun <solar3070@naver.com>
2022-07-17 03:11:58 +0900

    feat: MemoList 컴포넌트 추가

---
+++ src/components/learnDetail/MemoList.tsx
@@ -0,0 +1,83 @@
+import { MemoData } from '@src/services/api/types/learn-detail';
+import { COLOR } from '@src/styles/color';
+import { FONT_STYLES } from '@src/styles/fontStyle';
+import styled from 'styled-components';
+
+interface MemoListProps {
+  memoList: MemoData[];
+}
+
+function MemoList(props: MemoListProps) {
+  const { memoList } = props;
+
+  return (
+    <StMemoList>
+      {memoList.map(({ id, keyword, content }) => (
+        <StMemoWrapper key={id}>
+          <StMemoKeyword>{keyword.length > 28 ? `${keyword.slice(0, 27)}...` : keyword}</StMemoKeyword>
+          <StMemoContent>
+            {content.length > 30 ? (
+              <>
+                {keyword.slice(0, 26)}
+                <button type="button"> ... 더보기</button>
+              </>
+            ) : (
+              keyword
+            )}
+          </StMemoContent>
+        </StMemoWrapper>
+      ))}
+    </StMemoList>
+  );
+}
+
+export default MemoList;
+
+const StMemoList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+
+  height: 35.8rem;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 1rem;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: ${COLOR.GRAY_10};
+    border-radius: 1.3rem;
+  }
+`;
+
+const StMemoWrapper = styled.div`
+  margin-right: 0.8rem;
+  padding: 2.8rem 3.2rem 2.8rem 3.2rem;
+  width: 67rem;
+  height: 136px;
+
+  background-color: ${COLOR.SUB_BLUE_8};
+  border-radius: 2.5rem;
+`;
+
+const StMemoKeyword = styled.h1`
+  margin-bottom: 0.8rem;
+
+  color: ${COLOR.BLACK};
+  ${FONT_STYLES.SB_25_MEMO};
+`;
+
+const StMemoContent = styled.p`
+  color: ${COLOR.GRAY_80};
+  ${FONT_STYLES.R_23_MEMO};
+
+  & > button {
+    color: ${COLOR.GRAY_30};
+    ${FONT_STYLES.R_23_MEMO};
+
+    &:hover {
+      color: ${COLOR.MAIN_BLUE};
+    }
+  }
+`;
--- src/pages/learn/[id].tsx
+++ src/pages/learn/[id].tsx
@@ -8,12 +8,13 @@ import { COLOR } from '@src/styles/color';
 import { FONT_STYLES } from '@src/styles/fontStyle';
 import { GetServerSidePropsContext } from 'next';
 import { api } from '@src/services/api';
-import { VideoData } from '@src/services/api/types/learn-detail';
+import { MemoData, VideoData } from '@src/services/api/types/learn-detail';
 import YouTube from 'react-youtube';
 import EmptyMemo from '@src/components/learnDetail/EmptyMemo';
 import SEO from '@src/components/common/SEO';
+import MemoList from '@src/components/learnDetail/MemoList';
 
-function LearnDetail({ videoData }: { videoData: VideoData }) {
+function LearnDetail({ videoData, memoData }: { videoData: VideoData; memoData: MemoData[] }) {
   const router = useRouter();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [player, setPlayer] = useState<YT.Player | null>();
@@ -78,9 +79,7 @@ function LearnDetail({ videoData }: { videoData: VideoData }) {
                 <ImageDiv src={icMemo} className="memo" layout="fill" />
                 <h2>메모</h2>
               </StMemoTitle>
-              <StMemoWrapper>
-                <EmptyMemo />
-              </StMemoWrapper>
+              <StMemoWrapper>{memoData ? <MemoList memoList={memoData} /> : <EmptyMemo />}</StMemoWrapper>
             </StMemoContainer>
           </aside>
           <StLearnSection>
@@ -119,13 +118,15 @@ export default LearnDetail;
 
 export async function getServerSideProps({ params }: GetServerSidePropsContext) {
   const id = +(params?.id ?? -1);
-  const response = await api.learnDetailService.getVideoData(id);
-  if (response.id !== id) {
+  const videoData = await api.learnDetailService.getVideoData(id);
+  const memoData = await api.learnDetailService.getMemoData(id);
+
+  if (videoData.id !== id) {
     return {
       notFound: true,
     };
   }
-  return { props: { videoData: response } };
+  return { props: { videoData: videoData, memoData: memoData } };
 }
 
 const StLearnDetail = styled.div`
@@ -154,12 +155,14 @@ const StLearnDetail = styled.div`
 const StLearnMain = styled.main`
   display: flex;
   margin: 0 auto;
-  gap: 4.8rem;
+  gap: 4rem;
   width: 172rem;
   height: 111.9rem;
   padding: 8rem 8rem 0 8rem;
   border-radius: 3rem;
   background-color: ${COLOR.WHITE};
+
+  overflow: hidden;
 `;
 
 const StLearnSection = styled.section`
