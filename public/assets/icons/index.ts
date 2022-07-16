87a9279deba27f8f235b242ce12f7bcb5e373ad9
HyeJun <solar3070@naver.com>
2022-07-17 07:49:02 +0900

    asset: 메모 수정 취소, 완료 버튼 추가

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -5,6 +5,8 @@ export { default as icGuide } from './ic_guide.svg';
 export { default as icGuideHighlight } from './ic_guide_highlight.svg';
 export { default as icGuideSpacing } from './ic_guide_spacing.svg';
 export { default as icXButton } from './ic_x_button.svg';
+export { default as icMemoXButton } from './ic_memo_x_button.svg';
+export { default as icCheckButton } from './ic_check_button.svg';
 export { default as icEmptyBox } from './ic_empty_box.svg';
 export { default as icCheckedBox } from './ic_checked_box.svg';
 export { default as icScript } from './ic_script.svg';
