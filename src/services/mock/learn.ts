7ac87a03b6c917bae4bc109141d5043197ae907e
100Gyeon <bjy9966@naver.com>
2022-07-16 22:45:54 +0900

    feat: mock API 준비

--- src/services/mock/learn.ts
+++ src/services/mock/learn.ts
@@ -1,7 +1,7 @@
 import { LearnService } from '../api/learn';
 import { LEARN_DATA } from './learn.data';
 
-export function learnDetailDataMock(): LearnService {
+export function learnDataMock(): LearnService {
   const postSearchCondition = async () => {
     await wait(500);
     return LEARN_DATA.VIDEO_LIST_DATA;
