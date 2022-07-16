b00af0de732b27093ec2feccd7c8594efad383b0
100Gyeon <bjy9966@naver.com>
2022-07-16 18:10:12 +0900

    feat: twitter 관련 설정

--- src/components/common/SEO.tsx
+++ src/components/common/SEO.tsx
@@ -10,6 +10,10 @@ function SEO(props: SEOProps) {
   return (
     <Head>
       <title>{title}</title>
+      <meta
+        name="description"
+        content="뉴스 스크립트, 끊어 읽기, 하이라이트, 메모 남기기 등의 기능과 함께 아나운서의 발성, 발음을 따라하고 말하기 자신감을 키워요!"
+      />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="DeliverBle" />
       <meta property="og:title" content="딜리버블 | 아나운서 쉐도잉으로 키우는 스피치 자신감" />
@@ -21,8 +25,17 @@ function SEO(props: SEOProps) {
       <meta property="og:url" content="https://deliverble.vercel.app/" />
       <meta
         name="keywords"
-        content="뉴스 리딩, 뉴스 스크립트, 아나운서, 스피치, 말하기 학습, 발성, 끊어 읽기, 쉐도잉, 발음 교정, 발표 목소리"
+        content="뉴스 리딩, 스크립트, 아나운서, 스피치, 말하기 학습, 발성, 끊어 읽기, 쉐도잉, 발음 교정, 발표 목소리"
       />
+      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:site" content="DeliverBle" />
+      <meta property="twitter:title" content="딜리버블 | 아나운서 쉐도잉으로 키우는 스피치 자신감" />
+      <meta
+        property="twitter:description"
+        content="뉴스 스크립트, 끊어 읽기, 하이라이트, 메모 남기기 등의 기능과 함께 아나운서의 발성, 발음을 따라하고 말하기 자신감을 키워요!"
+      />
+      <meta property="twitter:image" content="/assets/images/img_thumbnail.png" />
+      <meta property="twitter:url" content="https://deliverble.vercel.app/" />
     </Head>
   );
 }
