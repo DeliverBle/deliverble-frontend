16dc21fca9a8cf5b60f68091f5ed291969fb46be
HyeJun <solar3070@naver.com>
2022-07-17 16:29:50 +0900

    feat: MemoPopup 컴포넌트 구현

--- src/components/learnDetail/MemoDotButton.tsx
+++ src/components/learnDetail/MemoDotButton.tsx
@@ -1,18 +1,25 @@
 import { icDotDefault, icDotHover } from 'public/assets/icons';
+import { useState } from 'react';
 import styled from 'styled-components';
 import ImageDiv from '../common/ImageDiv';
+import MemoPopup from './MemoPopup';
 
-function MemoDotButton() {
+interface MemoDotButtonProps {
+  editClicked: () => void;
+}
+
+function MemoDotButton(props: MemoDotButtonProps) {
+  const { editClicked } = props;
+
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
   return (
-    <StMemoDotButton
-      type="button"
-      onClick={() => {
-        console.log('test');
-      }}>
+    <StMemoDotButton type="button" onClick={() => setIsPopupOpen(true)}>
       <StMemoDotImage>
         <ImageDiv className="dot" src={icDotHover} alt="dot" />
         <ImageDiv className="dot default" src={icDotDefault} alt="like" />
       </StMemoDotImage>
+      {isPopupOpen && <MemoPopup openPopup={() => setIsPopupOpen(false)} editClicked={editClicked} />}
     </StMemoDotButton>
   );
 }
---
+++ src/components/learnDetail/MemoPopup.tsx
@@ -0,0 +1,51 @@
+import { COLOR } from '@src/styles/color';
+import { FONT_STYLES } from '@src/styles/fontStyle';
+import styled from 'styled-components';
+
+interface MemoPopupProps {
+  openPopup: () => void;
+  editClicked: () => void;
+}
+
+function MemoPopup(props: MemoPopupProps) {
+  const { openPopup, editClicked } = props;
+
+  return (
+    <StMemoPopup>
+      <StMemoPopupContent>
+        <StMemoEdit onClick={editClicked}>메모 수정</StMemoEdit>
+        <StMemoDelete>메모 삭제</StMemoDelete>
+      </StMemoPopupContent>
+    </StMemoPopup>
+  );
+}
+
+export default MemoPopup;
+
+const StMemoPopup = styled.div``;
+
+const StMemoPopupContent = styled.div`
+  position: absolute;
+  top: 4.8rem;
+  z-index: 100;
+
+  width: 10.3rem;
+  height: 8rem;
+
+  border: 1px solid ${COLOR.GRAY_10};
+  border-radius: 1.2rem;
+  background-color: ${COLOR.WHITE};
+`;
+
+const StMemoEdit = styled.button`
+  width: 9.1rem;
+  height: 3.2rem;
+
+  color: ${COLOR.BLACK};
+  ${FONT_STYLES.M_16_CAPTION};
+`;
+
+const StMemoDelete = styled.button`
+  color: ${COLOR.BLACK};
+  ${FONT_STYLES.M_16_CAPTION};
+`;
