7732d19f2eb3b8e7bdc97a5e8cb315a03034451e
100Gyeon <bjy9966@naver.com>
2022-07-18 20:36:22 +0900

    feat: VideoListSkeleton 구조화

---
+++ src/components/common/VideoListSkeleton.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import VideoItemSkeleton from './VideoItemSkeleton';
+import { COLOR } from '@src/styles/color';
+
+function VideoListSkeleton() {
+  return (
+    <StVideoListSkeleton>
+      <StHeader />
+      <div>
+        {new Array(12).fill('').map((_, i) => (
+          <VideoItemSkeleton key={i} />
+        ))}
+      </div>
+    </StVideoListSkeleton>
+  );
+}
+
+export default VideoListSkeleton;
+
+const StVideoListSkeleton = styled.div`
+  padding-top: 0.8rem;
+  margin-bottom: 45.2rem;
+
+  & > div {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-row-gap: 11.2rem;
+    grid-column-gap: 2rem;
+  }
+`;
+
+const StHeader = styled.div`
+  margin-bottom: 2.4rem;
+  width: 13rem;
+  height: 2rem;
+  background-color: ${COLOR.GRAY_5};
+  border-radius: 0.6rem;
+`;
