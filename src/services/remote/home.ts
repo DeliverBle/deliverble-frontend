9738f44294dc2cfc7803f28cc2db1af515c7196d
HyeJun <solar3070@naver.com>
2022-07-19 02:23:55 +0900

    feat: 서버 통신 성공 체크하는 코드 추가

--- src/services/remote/home.ts
+++ src/services/remote/home.ts
@@ -5,18 +5,20 @@ import { publicAPI } from './base';
 export function homeDataRemote(): HomeService {
   const getVideoData = async () => {
     const response = await publicAPI.get({ url: `/news/recommend` });
-    return {
-      videoList: response.data
-        ? response.data.map((video: VideoData) => ({
-            id: video.id,
-            title: video.title,
-            category: video.category,
-            channel: video.channel,
-            thumbnail: video.thumbnail,
-            reportDate: video.reportDate,
-          }))
-        : [],
-    };
+    if (response.status === 200) {
+      return {
+        videoList: response.data
+          ? response.data.map((video: VideoData) => ({
+              id: video.id,
+              title: video.title,
+              category: video.category,
+              channel: video.channel,
+              thumbnail: video.thumbnail,
+              reportDate: video.reportDate,
+            }))
+          : [],
+      };
+    } else throw '서버 통신 실패';
   };
 
   return { getVideoData };
