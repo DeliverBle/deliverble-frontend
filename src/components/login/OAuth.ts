f2144798e4c6af1a5b1c4794c7c44f9f07c9603a
choichoijin <cuj10594@yonsei.ac.kr>
2022-07-19 01:12:45 +0900

    chore: 연결 주소 실서버로 변경

--- src/components/login/OAuth.ts
+++ src/components/login/OAuth.ts
@@ -1,5 +1,1 @@
-//테스트 REST API
-const CLIENT_ID = '3c72adb081c3a1922f06614930f30782';
-const REDIRECT_URI = 'http://localhost:3000/kakao';
-
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const KAKAO_AUTH_URL = `https://deliverble.online/v2/auth/kakao`;
