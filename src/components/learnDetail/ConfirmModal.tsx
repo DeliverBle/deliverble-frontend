f0fe84104f58a707bad4e36e24d827fe447e990c
choichoijin <cuj10594@yonsei.ac.kr>
2022-07-18 21:50:26 +0900

    feat: 상황별 텍스트 추가

--- src/components/learnDetail/ConfirmModal.tsx
+++ src/components/learnDetail/ConfirmModal.tsx
@@ -8,15 +8,70 @@ interface ConfirmModalProps {
 
 function ConfirmModal(props: ConfirmModalProps) {
   const { closeModal } = props;
+
+  //ConfirmModal 생기는 상황별로 프롭스 가져오고 case로 나누기.
+  const situation: number = 1;
+  const mainText = () => {
+    switch (situation) {
+      case 1:
+        return '메모 작성을 취소하시겠습니까?';
+      case 2:
+        return '메모 수정을 취소하시겠습니까?';
+      case 3:
+        return '메모를 삭제하시겠습니까?';
+      case 4:
+        return '하이라이트를 삭제하시겠습니까?';
+    }
+  };
+
+  const subText = () => {
+    switch (situation) {
+      case 1:
+        return '작성 취소 선택시, 작성된 메모는 저장되지 않습니다.';
+      case 2:
+        return '수정 취소 선택시, 수정된 메모는 저장되지 않습니다.';
+      case 3:
+        return '';
+      case 4:
+        return '하이라이트 삭제시 해당 메모도 삭제됩니다.';
+    }
+  };
+
+  const cancelButtonText = () => {
+    switch (situation) {
+      case 1:
+        return '작성하기';
+      case 2:
+        return '계속하기';
+      case 3:
+        return '취소';
+      case 4:
+        return '취소';
+    }
+  };
+
+  const confirmButtonText = () => {
+    switch (situation) {
+      case 1:
+        return '작성 취소';
+      case 2:
+        return '수정 취소';
+      case 3:
+        return '삭제하기';
+      case 4:
+        return '삭제하기';
+    }
+  };
+
   return (
     <StConfirmModal>
       <StDescription>
-        <h2>하이라이트를 삭제하시겠습니까?</h2>
-        <p>작성 취소 선택시, 작성된 메모는 저장되지 않습니다.</p>
+        <h2>{mainText()}</h2>
+        <p>{subText()}</p>
       </StDescription>
       <StButtonContainer>
-        <button onClick={closeModal}>취소</button>
-        <button>삭제하기</button>
+        <button onClick={closeModal}>{cancelButtonText()}</button>
+        <button>{confirmButtonText()}</button>
       </StButtonContainer>
     </StConfirmModal>
   );
