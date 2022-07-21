3854e56c626b49b7b279cbd8d64a90d9bb4db013
HyeJun <solar3070@naver.com>
2022-07-22 08:42:43 +0900

    feat: 좋아요 목 데이터 추가

---
+++ src/services/remote/like.ts
@@ -0,0 +1,20 @@
+import { LikeData } from './../api/types/like';
+import { LikeService } from './../api/like';
+import { privateAPI } from './base';
+
+export function likeDataRemote(): LikeService {
+  const getLikeData = async () => {
+    const response = await privateAPI.get({ url: `/user/favorite/all` });
+    if (response.status === 200) {
+      return {
+        likeList: response.message
+          ? response.message.favoriteNews.map((like: LikeData) => ({
+              id: like.id,
+            }))
+          : [],
+      };
+    } else throw '서버 통신 실패';
+  };
+
+  return { getLikeData };
+}
