29c50eea2f57bfd8d78ca30bb28ee48e1c32dad9
songah <skutcm119@gmail.com>
2022-07-22 20:32:19 +0900

    chore: 타입추가

--- src/services/api/types/learn-detail.ts
+++ src/services/api/types/learn-detail.ts
@@ -28,3 +28,30 @@ export type MemoData = {
   keyword: string;
   content: string;
 };
+
+export type GroupedObjectKey = {
+  scriptId: string;
+};
+
+export type hlGroupedObjectList = {
+  endingIndex: number;
+  highlightId: number;
+  scriptId: number;
+  startingIndex: number;
+};
+
+export type spacingGroupedObjectList = {
+  spacingId: number;
+  scriptId: number;
+  index: number;
+};
+
+export type hlGroupedObject = {
+  objectIndex: GroupedObjectKey;
+  objectList: hlGroupedObjectList;
+};
+
+export type spacingGroupedObject = {
+  objectIndex: GroupedObjectKey;
+  objectList: spacingGroupedObjectList;
+};
