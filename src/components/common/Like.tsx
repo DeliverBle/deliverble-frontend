0ceb351da503b84657498057faf85ee8444bc990
HyeJun <solar3070@naver.com>
2022-07-23 07:51:39 +0900

    feat: 좋아요 클릭시 복습하기에 추가

--- src/components/common/Like.tsx
+++ src/components/common/Like.tsx
@@ -1,32 +1,70 @@
 import { icLikeClicked, icLikeHover, icLikeDefault } from 'public/assets/icons';
 import ImageDiv from './ImageDiv';
 import styled, { css } from 'styled-components';
+import { useEffect, useState } from 'react';
+import { api } from '@src/services/api';
 
 interface LikeProps {
   isFromList: boolean;
+  newsId: number;
   isLiked?: boolean;
-  toggleLike: () => void;
 }
 
 function Like(props: LikeProps) {
-  const { isFromList, isLiked, toggleLike } = props;
-  // const [isLikedTemp, setIsLikedTemp] = useState<boolean>(isLiked);
+  const { isFromList, newsId, isLiked } = props;
+  const [token, setToken] = useState('');
+  const [userId, setUserId] = useState('');
+  const [test, setTest] = useState(isLiked);
+  const [updateState, setUpdateState] = useState<number>(0);
 
-  // useEffect(() => {
-  //   setIsLikedTemp(isLiked);
-  // }, []);
+  useEffect(() => {
+    (async () => {
+      const getAccessToken = () => localStorage.getItem('token') ?? '';
+      const getUserId = () => localStorage.getItem('userId') ?? '';
+      if (getAccessToken()) {
+        setToken(getAccessToken());
+      }
+      if (getUserId()) {
+        setUserId(getUserId());
+      }
+    })();
+    setUpdateState(1);
+  }, []);
 
-  console.log('isLiked2', isLiked);
+  useEffect(() => {
+    setTest(isLiked);
+    setUpdateState(0);
+  }, [isLiked, test, updateState]);
+
+  const handleClick = async (newsId: number, test?: boolean) => {
+    if (test) {
+      console.log('데이터 삭제', test);
+      await api.likeService.deleteLikeData({
+        news_id: newsId,
+        access_token: token,
+        user_id: userId,
+      });
+      setTest(false);
+    } else {
+      console.log('데이터 추가', test);
+      await api.likeService.postLikeData({
+        news_id: newsId,
+        access_token: token,
+        user_id: userId,
+      });
+      setTest(true);
+    }
+  };
 
   return (
     <StLike
       type="button"
       onClick={(e) => {
         e.stopPropagation();
-        toggleLike();
+        handleClick(newsId, test);
       }}>
       <StLikeImage isFromList={isFromList}>
-        {isLiked ? (
+        {test ? (
           <ImageDiv className="like" src={icLikeClicked} alt="like" layout="fill" />
         ) : (
           <>
--- src/components/common/NewsList.tsx
+++ src/components/common/NewsList.tsx
@@ -15,69 +15,18 @@ interface NewsListProps {
 function NewsList(props: NewsListProps) {
   const { newsList } = props;
   const router = useRouter();
-  // const [likeList, setLikeList] = useState<number[]>([]);
-  const [token, setToken] = useState('');
-  const [userId, setUserId] = useState('');
-
-  const [, setUpdateState] = useState<number>(0);
+  const [, setUpdateState] = useState(0);
 
   useEffect(() => {
     (async () => {
       const { favoriteList } = await api.likeService.getLikeData();
-      // setLikeList(favoriteList.map((like) => like.id));
       console.log('favoriteList', favoriteList);
       newsList.map((news) => {
         news.isLiked = favoriteList.map((like) => like.id).includes(news.id) ? true : false;
       });
-      // console.log('newsList2', newsList);
       setUpdateState(1);
     })();
   }, []);
-
-  // useEffect(() => {
-  //   console.log('likeList', likeList);
-  //   // const tempList = JSON.parse(JSON.stringify(newsList));
-  //   // temp?.map((news: any) => {
-  //   //   news.isLiked = likeList?.includes(news.id) ? true : false;
-  //   // });
-  //   newsList?.map((news) => {
-  //     news.isLiked = likeList?.includes(news.id) ? true : false;
-  //   });
-  //   console.log('newsLisdsft', newsList[0].isLiked);
-  // }, [likeList, newsList]);
-
-  useEffect(() => {
-    (async () => {
-      const getAccessToken = () => localStorage.getItem('token') ?? '';
-      const getUserId = () => localStorage.getItem('userId') ?? '';
-      if (getAccessToken()) {
-        setToken(getAccessToken());
-      }
-      if (getUserId()) {
-        setUserId(getUserId());
-      }
-    })();
-  }, []);
-
-  const handleClick = async (id: number, isLiked?: boolean) => {
-    if (!isLiked) {
-      console.log('데이터 추가 요청');
-      await api.likeService.postLikeData({
-        news_id: id,
-        access_token: token,
-        user_id: userId,
-      });
-    } else {
-      console.log('데이터 삭제 요청');
-      await api.likeService.deleteLikeData({
-        news_id: id,
-        access_token: token,
-        user_id: userId,
-      });
-    }
-  };
-
-  // 버튼 클릭 -> 빈 버튼이었으면 즐찾 추가 (post) -> response를 가지고 화면에 꽉찬 하트 보여줌
 
   return (
     <StNewsList>
@@ -93,8 +42,7 @@ function NewsList(props: NewsListProps) {
                 layout="fill"
                 alt=""
               />
-              {console.log('isLiked', isLiked)}
-              <Like isFromList={true} isLiked={isLiked} toggleLike={() => handleClick(id, isLiked)} />
+              <Like isFromList={true} newsId={id} isLiked={isLiked} />
             </>
           </StThumbnail>
           <StTitle>{title}</StTitle>
