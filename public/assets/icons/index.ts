35f542f45fd014153f4d376ff7f1e599dea1d077
100Gyeon <bjy9966@naver.com>
2022-08-06 01:25:00 +0900

    asset: 도움말 버튼에 쓰이는 svg 추가 및 수정

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -2,6 +2,8 @@ export { default as icArrow } from './ic_arrow.svg';
 export { default as icDelete } from './ic_delete.svg';
 export { default as icSearch } from './ic_search.svg';
 export { default as icGuide } from './ic_guide.svg';
+export { default as icGuideHover } from './ic_guide_hover.svg';
+export { default as icGuideClicked } from './ic_guide_clicked.svg';
 export { default as icGuideHighlight } from './ic_guide_highlight.svg';
 export { default as icGuideSpacing } from './ic_guide_spacing.svg';
 export { default as icXButton } from './ic_x_button.svg';
