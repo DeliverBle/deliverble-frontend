aaf599d067b54f55608d9af6ed14475c5cc55fe3
100Gyeon <bjy9966@naver.com>
2022-08-10 03:45:04 +0900

    Revert "chore: 학습 상세 페이지에서 스타일 분리"
    This reverts commit f2cf52a453a95f1518daff96dd8090d51d73d548.

--- src/pages/learn/[id].tsx
+++ src/pages/learn/[id].tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { GetServerSidePropsContext } from 'next';
 import { useRouter } from 'next/router';
+import styled, { css } from 'styled-components';
 import YouTube from 'react-youtube';
 import SEO from '@src/components/common/SEO';
 import ImageDiv from '@src/components/common/ImageDiv';
@@ -15,6 +16,8 @@ import ConfirmModal from '@src/components/learnDetail/ConfirmModal';
 import LoginModal from '@src/components/login/LoginModal';
 import { api } from '@src/services/api';
 import { HighlightData, VideoData } from '@src/services/api/types/learn-detail';
+import { COLOR } from '@src/styles/color';
+import { FONT_STYLES } from '@src/styles/fontStyle';
 import {
   icXButton,
   icMemo,
@@ -26,19 +29,6 @@ import {
   icSpacingHover,
   icSpacingClicked,
 } from 'public/assets/icons';
-import {
-  StButton,
-  StButtonContainer,
-  StGuideButton,
-  StLearnDetail,
-  StLearnMain,
-  StLearnSection,
-  StMemoContainer,
-  StMemoTitle,
-  StMemoWrapper,
-  StScriptText,
-  StVideoWrapper,
-} from './style';
 
 function LearnDetail({ videoData, highlightData }: { videoData: VideoData; highlightData: HighlightData[] }) {
   const router = useRouter();
@@ -279,3 +269,239 @@ export async function getServerSideProps({ params }: GetServerSidePropsContext) 
   }
   return { props: { videoData: videoData, highlightData: highlightData } };
 }
+
+const StLearnDetail = styled.div`
+  padding: 16rem 10rem;
+  min-height: 100vh;
+  background: rgba(229, 238, 255, 0.85);
+  backdrop-filter: blur(2.8rem);
+
+  .close {
+    position: fixed;
+    top: 2.4rem;
+    right: 10rem;
+    width: 4.8rem;
+    height: 4.8rem;
+    cursor: pointer;
+  }
+
+  .guide {
+    position: relative;
+    width: 3.4rem;
+    height: 3.4rem;
+    cursor: pointer;
+  }
+`;
+
+const StGuideButton = styled.button<{ isModalOpen: boolean }>`
+  width: 3.2rem;
+  height: 3.2rem;
+  padding: 0;
+
+  &:hover {
+    background-image: url('/assets/icons/ic_guide_hover.svg');
+  }
+
+  ${({ isModalOpen }) =>
+    isModalOpen
+      ? css`
+          background-image: url('/assets/icons/ic_guide_clicked.svg');
+        `
+      : css`
+          background-image: url('/assets/icons/ic_guide.svg');
+        `}
+`;
+
+const StLearnMain = styled.main`
+  display: flex;
+  margin: 0 auto;
+  gap: 4rem;
+  width: 172rem;
+  height: 111.9rem;
+  padding: 8rem 8rem 0 8rem;
+  border-radius: 3rem;
+  background-color: ${COLOR.WHITE};
+  overflow: hidden;
+`;
+
+const StLearnSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 8rem;
+
+  & > div {
+    display: flex;
+    align-items: center;
+    gap: 1.2rem;
+    margin-top: 14rem;
+    margin-bottom: 2.4rem;
+
+    h2 {
+      color: ${COLOR.BLACK};
+      ${FONT_STYLES.SB_24_HEADLINE};
+    }
+  }
+
+  .announce {
+    position: relative;
+    width: 3.2rem;
+    height: 3.2rem;
+  }
+
+  article {
+    display: flex;
+    flex-direction: column;
+    width: 84.2rem;
+    height: 76rem;
+    padding: 1.8rem 1.2rem 1.8rem 2rem;
+    border: 0.2rem solid ${COLOR.GRAY_10};
+    border-radius: 2.4rem;
+    color: ${COLOR.BLACK};
+    font-size: 2.6rem;
+    line-height: 5.8rem;
+    word-break: keep-all;
+
+    div::selection {
+      background: ${COLOR.SUB_BLUE_30};
+    }
+
+    & > div:first-child {
+      position: relative;
+      flex: 1;
+      padding: 0.6rem 1.2rem;
+      height: 62.8rem;
+      overflow-y: scroll;
+
+      &::-webkit-scrollbar {
+        width: 1rem;
+      }
+
+      &::-webkit-scrollbar-thumb {
+        background-color: ${COLOR.GRAY_10};
+        border-radius: 1.3rem;
+      }
+
+      &::-webkit-scrollbar-track-piece {
+        height: 13.6rem;
+        max-height: 13.6rem;
+      }
+    }
+
+    & > div:last-child {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding-top: 1.8rem;
+      margin-top: 2.4rem;
+      border-top: 0.2rem solid ${COLOR.GRAY_10};
+    }
+  }
+`;
+
+const StScriptText = styled.div<{ isActive: boolean }>`
+  position: relative;
+  font-size: 2.6rem;
+  font-weight: ${({ isActive }) => (isActive ? 600 : 400)};
+  color: ${({ isActive }) => (isActive ? COLOR.MAIN_BLUE : COLOR.BLACK)};
+  cursor: pointer;
+
+  & > span {
+    font-size: 3.2rem;
+    font-weight: 600;
+    color: ${COLOR.MAIN_BLUE};
+    margin: 0 0.02rem 0 0.02rem;
+  }
+
+  & > mark {
+    background: linear-gradient(259.3deg, #d8d9ff 0%, #a7c5ff 100%);
+    font-weight: ${({ isActive }) => (isActive ? 600 : 400)};
+    color: ${({ isActive }) => (isActive ? COLOR.MAIN_BLUE : COLOR.BLACK)};
+
+    & > span {
+      font-size: 3.2rem;
+      font-weight: 600;
+      color: ${COLOR.MAIN_BLUE};
+    }
+  }
+
+  & > mark:hover {
+    color: ${COLOR.MAIN_BLUE};
+    font-weight: 600;
+  }
+`;
+
+const StButtonContainer = styled.div`
+  display: flex;
+  gap: 0.8rem;
+  position: relative;
+  padding-right: 0.8rem;
+`;
+
+const StButton = styled.button`
+  width: 4.8rem;
+  height: 4.8rem;
+
+  &:hover .default img {
+    transition: opacity 1s;
+    opacity: 0;
+  }
+
+  .function-button {
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+  }
+`;
+
+const StVideoWrapper = styled.div`
+  position: relative;
+  margin-bottom: 4.8rem;
+  width: fit-content;
+  height: fit-content;
+  border-radius: 2.4rem;
+  overflow: hidden;
+
+  .like-button {
+    position: absolute;
+    width: 4.8rem;
+    height: 4.8rem;
+    cursor: pointer;
+    top: 2.4rem;
+    right: 2.4rem;
+  }
+
+  video {
+    position: relative;
+    left: 0;
+    top: 0;
+    opacity: 1;
+  }
+`;
+
+const StMemoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StMemoTitle = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+  margin-bottom: 2.4rem;
+
+  & > h2 {
+    color: ${COLOR.BLACK};
+    ${FONT_STYLES.SB_24_HEADLINE};
+  }
+
+  .memo {
+    position: relative;
+    width: 3.2rem;
+    height: 3.2rem;
+  }
+`;
+
+const StMemoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
