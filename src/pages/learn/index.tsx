266fc7417c89bd3f093a0d5e2e3e73309504c2d6
100Gyeon <bjy9966@naver.com>
2022-08-10 04:14:07 +0900

    Revert "chore: 학습하기 페이지에서 스타일 분리"
    This reverts commit cb555538df2e810237d7911be71714a8e34cc69c.

--- src/pages/learn/index.tsx
+++ src/pages/learn/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import VideoListSkeleton from '@src/components/common/VideoListSkeleton';
 import SEO from '@src/components/common/SEO';
 import NavigationBar from '@src/components/common/NavigationBar';
@@ -7,10 +8,11 @@ import SelectBox from '@src/components/learn/SelectBox';
 import ImageDiv from '@src/components/common/ImageDiv';
 import Pagination from '@src/components/common/Pagination';
 import Footer from '@src/components/common/Footer';
+import { COLOR } from '@src/styles/color';
+import { FONT_STYLES } from '@src/styles/fontStyle';
 import { icSearch } from 'public/assets/icons';
 import { api } from '@src/services/api';
 import { VideoData } from '@src/services/api/types/home';
-import { StLearn, StResult, StSearch, StSelectBoxContainer, StTitle } from './style';
 
 const channelList = ['전체', 'SBS', 'KBS', 'MBC', '기타'];
 const categoryList = ['전체', '정치', '경제', '사회', '세계', '연예', '기타'];
@@ -122,3 +124,67 @@ function Learn() {
 }
 
 export default Learn;
+
+const StLearn = styled.div`
+  margin: auto 16rem;
+`;
+
+const StTitle = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.2rem;
+  margin-top: 16rem;
+  margin-bottom: 4.8rem;
+
+  .search {
+    position: relative;
+    width: 4.8rem;
+    height: 4.8rem;
+  }
+
+  & > h1 {
+    ${FONT_STYLES.SB_32_HEADLINE};
+    color: ${COLOR.BLACK};
+  }
+`;
+
+const StSearch = styled.div`
+  display: flex;
+  align-items: flex-end;
+  margin-bottom: 8rem;
+
+  & > button {
+    background-color: ${COLOR.MAIN_BLUE};
+    color: ${COLOR.WHITE};
+    ${FONT_STYLES.B_20_BODY};
+    padding: 1.4rem 4rem 1.4rem 3.9rem;
+    border-radius: 1.4rem;
+    margin-left: 4rem;
+    min-width: fit-content;
+    height: 5.6rem;
+  }
+`;
+
+const StSelectBoxContainer = styled.div`
+  display: flex;
+  gap: 1.6rem;
+`;
+
+const StResult = styled.div`
+  & > h2 {
+    ${FONT_STYLES.M_20_BODY};
+    color: ${COLOR.GRAY_30};
+    margin-bottom: 2.3rem;
+
+    span {
+      color: ${COLOR.MAIN_BLUE};
+      font-weight: 600;
+    }
+  }
+
+  & > div {
+    margin-top: 16rem;
+    margin-bottom: 26.4rem;
+    text-align: center;
+  }
+`;
