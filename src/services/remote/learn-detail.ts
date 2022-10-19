24ac6d3eb3ba50d924b0935d45893235aaebadca
100Gyeon <bjy9966@naver.com>
2022-10-20 04:52:59 +0900

    refactor: Get News Detail (authenticated) API 연결

--- src/services/remote/learn-detail.ts
+++ src/services/remote/learn-detail.ts
@@ -1,11 +1,11 @@
 import { LearnDetailService } from '../api/learn-detail';
 import { Script, Tag } from '../api/types/learn-detail';
 import { LEARN_DETAIL_DATA } from '../mock/learn-detail.data';
-import { publicAPI } from './base';
+import { privateAPI } from './base';
 
 export function learnDetailDataRemote(): LearnDetailService {
   const getVideoData = async (id: number) => {
-    const response = await publicAPI.get({ url: `/news/detail/${id}` });
+    const response = await privateAPI.get({ url: `/news/detail/${id}` });
     if (response.status === 200) {
       return {
         id: response.data.id,
@@ -16,15 +16,15 @@ export function learnDetailDataRemote(): LearnDetailService {
         reportDate: response.data.reportDate,
         startTime: response.data.startTime,
         endTime: response.data.endTime,
-        tags: response.data.tags.map((tag: Tag) => ({
+        tags: response.data.tagsForView.map((tag: Tag) => ({
           id: tag.id,
           name: tag.name,
         })),
-        scripts: response.data.scripts.map((script: Script) => ({
-          id: script.id,
-          text: script.text,
-          startTime: script.startTime,
-          endTime: script.endTime,
+        scripts: response.data2[0].sentences.map((sentence: Script) => ({
+          id: sentence.id,
+          text: sentence.text,
+          startTime: sentence.startTime,
+          endTime: sentence.endTime,
         })),
       };
     } else throw '서버 통신 실패';
