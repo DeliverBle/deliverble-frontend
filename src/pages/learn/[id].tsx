27da8edf364ceeb5fca234eaf09381a65e032558
songah <skutcm119@gmail.com>
2022-11-01 12:40:54 +0900

    [chore]태그 변경

--- src/pages/learn/[id].tsx
+++ src/pages/learn/[id].tsx
@@ -177,7 +177,7 @@ function LearnDetail({ highlightData }: { highlightData: HighlightData[] }) {
                           key={id}
                           onClick={() => player?.seekTo(startTime, true)}
                           isActive={startTime <= currentTime && currentTime < endTime ? true : false}>
-                          <p id={id.toString()}>{<div dangerouslySetInnerHTML={{ __html: text }}></div>}</p>
+                          <div id={id.toString()} dangerouslySetInnerHTML={{ __html: text }}></div>
                           {clickedHighlightId === id && !newMemoHighlightId && !editMemoHighlightId && (
                             <ContextMenu
                               points={points}
