4825b3e066166a45878a1cc2b748b5a767803e4e
songah <skutcm119@gmail.com>
2022-11-08 22:45:36 +0900

    [chore] 주석 삭제 및 함수,변수 정렬

--- src/components/learnDetail/ScriptEdit.tsx
+++ src/components/learnDetail/ScriptEdit.tsx
@@ -14,16 +14,29 @@ interface ScriptEditProps {
 function ScriptEdit(props: ScriptEditProps) {
   const { scriptsId, scripts, isHighlight, isSpacing } = props;
   const [highlightAlert, setHighlightAlert] = useState<boolean>(false);
-  const [firstLineId, setFirstLineId] = useState<number>(); // 첫번째 줄의 id값
+  const [firstLineId, setFirstLineId] = useState<number>();
   const [order, setOrder] = useState<number>();
+  const [resText, setResText] = useState<string>();
   const learnRef = useRef<HTMLDivElement>(null);
 
-  //order을 구하기 위해 첫번째 라인의 id를 저장하는 코드
+  useEffect(() => {
+    (async () => {
+      if (order && resText && scriptsId) {
+        await api.learnDetailService.postSentenceData(
+          {
+            order: order,
+            text: resText,
+          },
+          scriptsId,
+        );
+      }
+    })();
+  }, [order, resText, scriptsId]);
+
   useEffect(() => {
     setFirstLineId(scripts[0].id);
   }, [firstLineId, scripts]);
 
-  //order을 구하는 함수
   const findLineOrder = (currentLineId: number | undefined) => {
     if (currentLineId && firstLineId) {
       const order = currentLineId - firstLineId + 1;
@@ -53,16 +66,13 @@ function ScriptEdit(props: ScriptEditProps) {
     const serializer = new XMLSerializer();
     const selectedLine = serializer.serializeToString(selectedDiv);
 
-    //빈칸인 지 빈칸의 왼쪽, 오른쪽 탐지 해서 중간 위치하도록
     const isLeftBlank = startIdx && selectedLine[startIdx - 1] === ' ';
     const isRightBlank = startIdx && selectedLine[startIdx] === ' ';
     const isValidate = isLeftBlank || isRightBlank;
 
-    //하이라이트 중복 검사
     let isOverlap = false;
     if (selection?.type === 'Range') {
-      const marks = document.getElementsByTagName('mark'); //mark라는 태그를 모두 담은 html collection
-      // marks를 돌면서 현재 셀렉션 중 marks와 겹치는 것이 있는지 확인
+      const marks = document.getElementsByTagName('mark');
       for (let i = 0; i < marks.length; i++) {
         if (selection?.containsNode(marks[i], true) === true) {
           isOverlap = true;
@@ -72,7 +82,6 @@ function ScriptEdit(props: ScriptEditProps) {
       }
     }
 
-    //끊어읽기 표시
     if (selection?.type === 'Caret' && isValidate && isSpacing) {
       const frag = document.createDocumentFragment();
       const div = document.createElement('div');
@@ -83,16 +92,13 @@ function ScriptEdit(props: ScriptEditProps) {
       }
       range?.insertNode(frag);
     } else if (!isOverlap && selection?.type === 'Range' && isHighlight) {
-      //하이라이트 표시
       let text = selection.toString();
 
-      //문장간 하이라이팅의 경우 조기 return
       if (text.includes('\n')) {
         selection?.collapseToEnd();
         return;
       }
 
-      //하이라이트 중간에 끊어읽기 표시 들어가있으면 span 넣은걸로 대체하기
       if (text.includes('/')) {
         const texts = text.split('/');
         text = texts.join('<span>/</span>');
@@ -113,13 +119,8 @@ function ScriptEdit(props: ScriptEditProps) {
   };
 
   let isHiglightOverSpacing = false;
-
-  //수정된 html을 text로 직렬화 하는 함수
-  const [resText, setResText] = useState<string>();
   const nodeToText = (anchorNode: Node | null | undefined) => {
     let textVal = '';
-    //끊어읽기를 하이라이트 안에 표시한 경우 anchor노드가 하이라이트로 잡혀서
-    //anchorNode의 부모노드로 다시 호출해주었습니다.
     if (anchorNode?.nodeName === 'MARK') {
       nodeToText(anchorNode.parentNode);
       isHiglightOverSpacing = true;
@@ -128,7 +129,6 @@ function ScriptEdit(props: ScriptEditProps) {
 
     isHiglightOverSpacing = false;
     if (!isHiglightOverSpacing && anchorNode?.childNodes) {
-      //노드를 순회하면서 text이면 그대로 text를 복사해주고 하이라이트이면 태그를 붙여서 복사해주고 /이면 span을 붙여서 복사
       for (let i = 0; i < anchorNode?.childNodes.length; i++) {
         switch (anchorNode?.childNodes[i].nodeName) {
           case '#text':
@@ -136,7 +136,6 @@ function ScriptEdit(props: ScriptEditProps) {
             break;
 
           case 'MARK':
-            //하이라이트 안에 끊어읽기가 포함되는 경우 예외처리
             if (anchorNode?.childNodes[i].textContent?.includes('/')) {
               let text = anchorNode?.childNodes[i].textContent;
               if (text) {
@@ -158,21 +157,6 @@ function ScriptEdit(props: ScriptEditProps) {
       setResText(textVal);
     }
   };
-
-  //order과 text post 하는 함수
-  useEffect(() => {
-    (async () => {
-      if (order && resText && scriptsId) {
-        await api.learnDetailService.postSentenceData(
-          {
-            order: order,
-            text: resText,
-          },
-          scriptsId,
-        );
-      }
-    })();
-  }, [order, resText, scriptsId]);
 
   return (
     <>
