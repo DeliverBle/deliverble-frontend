9181f1a78776bbe8da40f07e397c8cd419609afd
songah <skutcm119@gmail.com>
2022-12-27 19:22:50 +0900

    chore: 서버 목

--- src/services/mock/review.ts
+++ src/services/mock/review.ts
@@ -7,7 +7,12 @@ export function reviewDataMock(): ReviewService {
     return REVIEW_DATA.FAVORITE_LIST_DATA;
   };
 
-  return { postFavoriteVideoList };
+  const postHistoryVideoList = async () => {
+    await wait(500);
+    return REVIEW_DATA.HISTORY_LIST_DATA;
+  };
+
+  return { postFavoriteVideoList, postHistoryVideoList };
 }
 
 const wait = (milliSeconds: number) => new Promise((resolve) => setTimeout(resolve, milliSeconds));
