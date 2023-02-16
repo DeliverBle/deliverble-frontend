3c16c517bfc815c7df175fbda4cd5b0a8e715cbc
100Gyeon <bjy9966@naver.com>
2023-02-16 21:49:19 +0900

    feat: HTTP 상태 코드 상수화

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -89,3 +89,11 @@ export const INITIAL_MEMO = {
   keyword: '',
   highlightId: '',
 };
+
+export const STATUS_CODE = {
+  OK: 200,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  INTERNAL_SERVER_ERROR: 500,
+};
