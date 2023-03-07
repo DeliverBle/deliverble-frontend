ead71ad0e619b0ac5ee6c6e35a8846ff8e285352
100Gyeon <bjy9966@naver.com>
2023-03-07 19:33:35 +0900

    feat: @next/bundle-analyzer 설치 및 적용

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
@@ -20,4 +25,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
