2c728e5d949a52e304ba3b4102d65bdd64c86a21
hyejun <solar3070@naver.com>
2023-04-15 03:26:10 +0900

    fix: mock 데이터, 코드 수정

--- src/services/remote/common.ts
+++ src/services/remote/common.ts
@@ -5,7 +5,8 @@ import { API } from './base';
 export function commonDataRemote(): CommonService {
   const requestLogin = async (code: string) => {
     try {
-      const response = await axios.post(`https://deliverble.online/auth/authentication/kakao?code=${code}`);
+      // const response = await axios.post(`https://deliverble.online/auth/authentication/kakao?code=${code}`);
+      const response = await axios.post(`http://13.209.32.166:8081/auth/authentication/kakao?code=${code}`);
       return response.data.data.accessToken;
     } catch {
       throw '로그인 에러 발생';
@@ -28,12 +29,7 @@ export function commonDataRemote(): CommonService {
 
   const postLikeData = async (newsId: number) => {
     const response = await API.post({ url: `/user/favorite/${newsId}` });
-    if (response.statusCode === 200) {
-      return {
-        id: response.data.newsId,
-        isFavorite: response.data.isFavorite,
-      };
-    } else throw '서버 통신 실패';
+    return response.data;
   };
 
   return { requestLogin, getUserInfo, postLikeData };
