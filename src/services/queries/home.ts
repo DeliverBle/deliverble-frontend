2f413bd36284e2a3c75d28096f37a3d36fc4a3dc
hyejun <solar3070@naver.com>
2023-04-18 12:33:40 +0900

    refactor: 추천 뉴스, 스피치 가이드 staleTime 무한 지정

--- src/services/queries/home.ts
+++ src/services/queries/home.ts
@@ -2,9 +2,15 @@ import { api } from '@src/services/api';
 import { useQuery } from '@tanstack/react-query';
 
 export const useGetRecommendVideoList = () => {
-  return useQuery(['getRecommendVideoList'], () => api.homeService.getVideoData());
+  return useQuery(['getRecommendVideoList'], () => api.homeService.getVideoData(), {
+    cacheTime: Infinity,
+    staleTime: Infinity,
+  });
 };
 
 export const useGetSpeechGuideList = () => {
-  return useQuery(['getSpeechGuideList'], () => api.homeService.getSpeechGuideData());
+  return useQuery(['getSpeechGuideList'], () => api.homeService.getSpeechGuideData(), {
+    cacheTime: Infinity,
+    staleTime: Infinity,
+  });
 };
