f532f0fc69b5a3feb8c10bdf14f6f0adee6ad60e
hyejun <solar3070@naver.com>
2023-04-18 12:34:13 +0900

    refactor: 뉴스 필터링 invalidation 대신 setQueryData 사용

--- src/services/queries/learn.ts
+++ src/services/queries/learn.ts
@@ -4,11 +4,14 @@ import { useMutation, useQuery } from '@tanstack/react-query';
 import { queryClient } from '../../pages/_app';
 
 export const useGetSearchCondition = (data: PostSearchConditionRequestBody) => {
-  return useQuery(['postSearchCondition', data], () => api.learnService.postSearchCondition(data));
+  return useQuery(['postSearchCondition', data], () => api.learnService.postSearchCondition(data), {
+    enabled: false,
+    keepPreviousData: true,
+  });
 };
 
 export const usePostSearchCondition = () => {
   return useMutation(api.learnService.postSearchCondition, {
-    onSuccess: () => queryClient.invalidateQueries(['postSearchCondition']),
+    onSuccess: (data, variables) => queryClient.setQueryData(['postSearchCondition', variables], data),
   });
 };
