041fc246b62ce6c9ae64c6a1bad1e3aed2c85205
hyejun <solar3070@naver.com>
2023-05-10 14:40:27 +0900

    refactor: 폰트 preload 및 가변 다이나믹 서브셋으로 변경

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -28,7 +28,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body, button, textarea, input {
-    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+    font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   }
 
   body {
