7473ea6e8533a1b437de7128fe13dc5e33456b90
hyejun <solar3070@naver.com>
2023-06-08 12:15:35 +0900

    chore: jest 설정

---
+++ jest.setup.js
@@ -0,0 +1,22 @@
+// Optional: configure or set up a testing framework before each test.
+// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+
+// Used for __tests__/testing-library.js
+// Learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect';
+import 'jest-canvas-mock';
+import { server } from '@src/mocks/server';
+
+jest.mock('swiper/css', jest.fn());
+
+// Establish API mocking before all tests.
+beforeAll(() => server.listen());
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers());
+
+// Clean up after the tests are finished.
+afterAll(() => server.close());
+
+export {};
