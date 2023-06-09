35061bc60310e65a08f42b596ee8c984e0db006e
hyejun <solar3070@naver.com>
2023-06-09 15:56:55 +0900

    chore: jest 설정 파일 수정

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,12 @@
 const nextJest = require('next/jest');
 
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-});
+const createJestConfig = nextJest({ dir: './' });
 
-// Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '^@src/(.*)$': '<rootDir>/src/$1',
-    '^public/(.*)$': '<rootDir>/public/$1',
-  },
+  moduleNameMapper: { '^@src/(.*)$': '<rootDir>/src/$1', '^public/(.*)$': '<rootDir>/public/$1' },
   transformIgnorePatterns: ['node_modules'],
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
--- jest.setup.js
+++ jest.setup.js
@@ -1,27 +1,10 @@
-// Optional: configure or set up a testing framework before each test.
-// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
-
-// Used for __tests__/testing-library.js
-// Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-canvas-mock';
-import { server } from '@src/mocks/server';
 
 jest.mock('swiper/css', jest.fn());
-
-// Establish API mocking before all tests.
-beforeAll(() => server.listen());
-
-// Reset any request handlers that we may add during the tests,
-// so they don't affect other tests.
-afterEach(() => server.resetHandlers());
-
-// Clean up after the tests are finished.
-afterAll(() => server.close());
 
 const portalRoot = document.createElement('div');
 portalRoot.setAttribute('id', 'portal');
 document.body.appendChild(portalRoot);
 
-// JSDOM does not implement global "scrollTo" function
 window.scrollTo = jest.fn();
