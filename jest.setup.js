98fe804096570d8bf3303ccbb0857b95db38c4bd
hyejun <solar3070@naver.com>
2023-06-12 19:26:14 +0900

    chore: msw 셋업

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-canvas-mock';
+import { server } from '@src/mocks/server';
 
 jest.mock('swiper/css', jest.fn());
 
@@ -8,3 +9,7 @@ portalRoot.setAttribute('id', 'portal');
 document.body.appendChild(portalRoot);
 
 window.scrollTo = jest.fn();
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
