82dd00ed5757b8252cb139f2763e5315538e3793
hyejun <solar3070@naver.com>
2023-06-12 16:04:38 +0900

    feat: 메모 수정 관련 ConfirmModal 유닛 테스트 작성

--- __tests__/learnDetail/ConfirmModal.test.tsx
+++ __tests__/learnDetail/ConfirmModal.test.tsx
@@ -1,16 +1,17 @@
 import { ConfirmModal } from '@src/components/learnDetail/modal';
-import { NEW_MEMO_MODAL_TEXT } from '@src/constants/learnDetail/modal';
+import { INITIAL_MEMO_STATE } from '@src/constants/learnDetail/memo';
+import { EDIT_MEMO_MODAL_TEXT, NEW_MEMO_MODAL_TEXT } from '@src/constants/learnDetail/modal';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-describe('메모 추가 확인 모달 오픈', () => {
-  const setup = async () => {
-    const setMemoState = jest.fn();
-    const setIsConfirmOpen = jest.fn();
-    const onTitleDelete = jest.fn();
-    const updateMemoList = jest.fn();
-    const cancelCreateMemo = jest.fn();
+const setMemoState = jest.fn();
+const setIsConfirmOpen = jest.fn();
+const onTitleDelete = jest.fn();
+const updateMemoList = jest.fn();
+const cancelCreateMemo = jest.fn();
 
+describe('메모 추가 확인 모달 오픈', () => {
+  const setup = () => {
     const { getByText } = render(
       <ConfirmModal
         confirmModalText={NEW_MEMO_MODAL_TEXT}
@@ -25,20 +26,54 @@ describe('메모 추가 확인 모달 오픈', () => {
     const leftButton = getByText(NEW_MEMO_MODAL_TEXT.leftButtonText);
     const rightButton = getByText(NEW_MEMO_MODAL_TEXT.rightButtonText);
 
-    return { setIsConfirmOpen, cancelCreateMemo, leftButton, rightButton };
+    return { leftButton, rightButton };
   };
 
   it('작성하기 버튼 클릭시 모달 클로즈', async () => {
-    const { setIsConfirmOpen, leftButton } = await setup();
+    const { leftButton } = setup();
 
     await userEvent.click(leftButton);
     expect(setIsConfirmOpen).toHaveBeenCalledWith(false);
   });
 
   it('취소 버튼 클릭시 메모 작성 취소', async () => {
-    const { cancelCreateMemo, rightButton } = await setup();
+    const { rightButton } = setup();
 
     await userEvent.click(rightButton);
     expect(cancelCreateMemo).toHaveBeenCalled();
+  });
+});
+
+describe('메모 수정 확인 모달 오픈', () => {
+  const setup = () => {
+    const { getByText } = render(
+      <ConfirmModal
+        confirmModalText={EDIT_MEMO_MODAL_TEXT}
+        setMemoState={setMemoState}
+        setIsConfirmOpen={setIsConfirmOpen}
+        onTitleDelete={onTitleDelete}
+        updateMemoList={updateMemoList}
+        cancelCreateMemo={cancelCreateMemo}
+      />,
+    );
+
+    const leftButton = getByText(EDIT_MEMO_MODAL_TEXT.leftButtonText);
+    const rightButton = getByText(EDIT_MEMO_MODAL_TEXT.rightButtonText);
+
+    return { leftButton, rightButton };
+  };
+
+  it('수정하기 버튼 클릭시 모달 클로즈', async () => {
+    const { leftButton } = setup();
+
+    await userEvent.click(leftButton);
+    expect(setIsConfirmOpen).toHaveBeenCalledWith(false);
+  });
+
+  it('수정 취소 버튼 클릭시 메모 수정 취소', async () => {
+    const { rightButton } = setup();
+
+    await userEvent.click(rightButton);
+    expect(setMemoState).toHaveBeenCalledWith(INITIAL_MEMO_STATE);
   });
 });
