49e29473434691110be298ba1290f20ff5e37075
hyejun <solar3070@naver.com>
2023-06-12 16:55:34 +0900

    feat: 메모 삭제 관련 ConfirmModal 유닛 테스트 작성

--- __tests__/learnDetail/ConfirmModal.test.tsx
+++ __tests__/learnDetail/ConfirmModal.test.tsx
@@ -1,6 +1,6 @@
 import { ConfirmModal } from '@src/components/learnDetail/modal';
 import { INITIAL_MEMO_STATE } from '@src/constants/learnDetail/memo';
-import { EDIT_MEMO_MODAL_TEXT, NEW_MEMO_MODAL_TEXT } from '@src/constants/learnDetail/modal';
+import { DELETE_MEMO_MODAL_TEXT, EDIT_MEMO_MODAL_TEXT, NEW_MEMO_MODAL_TEXT } from '@src/constants/learnDetail/modal';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
@@ -75,5 +75,39 @@ describe('메모 수정 확인 모달 오픈', () => {
 
     await userEvent.click(rightButton);
     expect(setMemoState).toHaveBeenCalledWith(INITIAL_MEMO_STATE);
+  });
+});
+
+describe('메모 삭제 확인 모달 오픈', () => {
+  const setup = () => {
+    const { getByText } = render(
+      <ConfirmModal
+        confirmModalText={DELETE_MEMO_MODAL_TEXT}
+        setMemoState={setMemoState}
+        setIsConfirmOpen={setIsConfirmOpen}
+        onTitleDelete={onTitleDelete}
+        updateMemoList={updateMemoList}
+        cancelCreateMemo={cancelCreateMemo}
+      />,
+    );
+
+    const leftButton = getByText(DELETE_MEMO_MODAL_TEXT.leftButtonText);
+    const rightButton = getByText(DELETE_MEMO_MODAL_TEXT.rightButtonText);
+
+    return { leftButton, rightButton };
+  };
+
+  it('취소 버튼 클릭시 모달 클로즈', async () => {
+    const { leftButton } = setup();
+
+    await userEvent.click(leftButton);
+    expect(setIsConfirmOpen).toHaveBeenCalledWith(false);
+  });
+
+  it('삭제하기 버튼 클릭시 메모 삭제', async () => {
+    const { rightButton } = setup();
+
+    await userEvent.click(rightButton);
+    expect(updateMemoList).toHaveBeenCalledWith('delete');
   });
 });
