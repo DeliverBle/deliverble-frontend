141e776f12df462f11addc6a35834c8255ddfc2d
hyejun <solar3070@naver.com>
2023-06-13 18:32:12 +0900

    feat: 뉴스 상세 정보 API 모킹

---
+++ src/mocks/api/data/learnDetailData.ts
@@ -0,0 +1,119 @@
+export const videoData = {
+  data: {
+    id: 77,
+    title: 'BTS "그래미 눈물났지만 언제든 도전 가능"...병역 조속 결론 촉구',
+    category: '연예',
+    announcerGender: '여성',
+    channel: '기타',
+    link: 'gBX4lQoV6cc',
+    thumbnail: 'https://img.youtube.com/vi/gBX4lQoV6cc/hqdefault.jpg',
+    startTime: 0,
+    endTime: 20,
+    suitability: '상',
+    isEmbeddable: 'True',
+    reportDate: '2022-04-10',
+    isFavorite: false,
+    tagsForView: [
+      {
+        name: 'BTS',
+        category: '연예',
+        id: 120,
+      },
+      {
+        name: '그래미',
+        category: '기타',
+        id: 182,
+      },
+      {
+        name: '병역',
+        category: '기타',
+        id: 243,
+      },
+    ],
+    haveGuide: false, // true
+  },
+  data2: [
+    {
+      id: 77,
+      newsId: 77,
+      name: '스크립트 1',
+      sentences: [
+        {
+          id: 344,
+          order: 1,
+          startTime: 0,
+          endTime: 8.69,
+          text: '그룹 방탄소년단, BTS의 미국 라스베이거스 공연이 이어지면서 오는 주말까지 팬 수십만 명이 몰려들 것으로 보입니다.',
+        },
+        {
+          id: 345,
+          order: 2,
+          startTime: 8.69,
+          endTime: 13.44,
+          text: 'BTS와 소속사 하이브가 현지에서 모처럼 만에 기자회견을 열었는데요.',
+        },
+        {
+          id: 346,
+          order: 3,
+          startTime: 13.44,
+          endTime: 17.62,
+          text: '그래미 시상식과 병역 문제와 관련한 입장을 언급했습니다.',
+        },
+        {
+          id: 347,
+          order: 4,
+          startTime: 17.62,
+          endTime: 20,
+          text: '박기완 기자입니다.',
+        },
+      ],
+    },
+  ],
+};
+
+export const similarVideoData = {
+  data: {
+    exploreNewsDtoCollection: [
+      {
+        id: 12,
+        title: '손님들이 건넨 술 마시고 사망…함께 있던 남성은 사고사',
+        category: '사회',
+        channel: 'SBS',
+        thumbnail: 'https://img.youtube.com/vi/mDW6crrUVpA/hqdefault.jpg',
+        reportDate: '2022-07-06',
+        isFavorite: true,
+        haveGuide: false,
+      },
+      {
+        id: 8,
+        title: "우크라이나 재건회의 '루가노 선언' 채택",
+        category: '세계',
+        channel: 'MBC',
+        thumbnail: 'https://img.youtube.com/vi/1SRz_AE8R9E/hqdefault.jpg',
+        reportDate: '2022-07-06',
+        isFavorite: true,
+        haveGuide: false,
+      },
+      {
+        id: 3,
+        title: '제주, 초중고교 무상급식 예산 46억 원 증액 ',
+        category: '사회',
+        channel: 'SBS',
+        thumbnail: 'https://img.youtube.com/vi/45IAfzlB_tQ/hqdefault.jpg',
+        reportDate: '2022-07-06',
+        isFavorite: false,
+        haveGuide: false,
+      },
+      {
+        id: 4,
+        title: '안동시 공무원, 동료 직원 흉기에 찔려 사망',
+        category: '사회',
+        channel: 'SBS',
+        thumbnail: 'https://img.youtube.com/vi/sId-zbgWuZU/hqdefault.jpg',
+        reportDate: '2022-07-05',
+        isFavorite: false,
+        haveGuide: false,
+      },
+    ],
+  },
+};
