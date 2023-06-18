8e51c5f50ba2f19523dd5b9426b85b1397a6a427
hyejun <solar3070@naver.com>
2023-06-18 20:00:44 +0900

    chore: console.log 제거

--- src/mocks/api/learnDetail.ts
+++ src/mocks/api/learnDetail.ts
@@ -3,27 +3,22 @@ import { rest } from 'msw';
 
 const handlers = [
   rest.get('https://deliverble.online/news/similar/2', (_, res, ctx) => {
-    console.log('비슷한 뉴스 추천 mock API 호출');
     return res(ctx.status(200), ctx.json(similarVideoData));
   }),
 
   rest.get('https://deliverble.online/news/detail/not-authentication/2', (_, res, ctx) => {
-    console.log('뉴스 상세 정보 mock API 호출 (비인증)');
     return res(ctx.status(200), ctx.json(videoData));
   }),
 
   rest.get('https://deliverble.online/news/detail/2', (_, res, ctx) => {
-    console.log('뉴스 상세 정보 mock API 호출 (인증)');
     return res(ctx.status(200), ctx.json(videoData));
   }),
 
   rest.post(`https://deliverble.online/script/memo/create/197`, (_, res, ctx) => {
-    console.log('메모 추가 mock API 호출');
     return res(ctx.status(200), ctx.json(createMemo));
   }),
 
   rest.patch(`https://deliverble.online/script/memo/update/3`, (_, res, ctx) => {
-    console.log('메모 수정 mock API 호출');
     return res(ctx.status(200), ctx.json(updateMemo));
   }),
 ];
