a6c74ae80532e51dfccdc8c18f43c289988aceeb
hyejun <solar3070@naver.com>
2023-06-18 16:44:17 +0900

    chore: 쿼리 캐시 클리어 설정 추가

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-canvas-mock';
 import { server } from '@src/mocks/server';
+import { QueryCache } from '@tanstack/react-query';
 
 jest.mock('swiper/css', jest.fn());
 
@@ -10,6 +11,11 @@ document.body.appendChild(portalRoot);
 
 window.scrollTo = jest.fn();
 
+const queryCache = new QueryCache();
+
 beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
+afterEach(() => {
+  server.resetHandlers();
+  queryCache.clear();
+});
 afterAll(() => server.close());
