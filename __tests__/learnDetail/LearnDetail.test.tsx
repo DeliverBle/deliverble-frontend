b803da2e81d3647c46bf15356f981d1a4e6fea49
hyejun <solar3070@naver.com>
2023-06-18 19:22:04 +0900

    refactor: 문자열 상수화

--- __tests__/learnDetail/LearnDetail.test.tsx
+++ __tests__/learnDetail/LearnDetail.test.tsx
@@ -21,78 +21,93 @@ jest.mock('next/router', () => ({
 
 localStorage.setItem('recoil-persist', '{ "loginState": true }');
 
+const setup = () => {
+  const { getByText, getAllByText, getByRole, queryByText, findByText, findByRole } = render(<LearnDetail />, {
+    wrapper: createWrapper(),
+  });
+
+  return { getByText, getAllByText, getByRole, queryByText, findByText, findByRole };
+};
+
+const SIMILAR_NEWS_TITLE = "우크라이나 재건회의 '루가노 선언' 채택";
+const NEWS_TITLE = '북, 최근 임진강 상류 황강댐 수문 개방';
+
 describe('학습 상세 페이지 목 API 호출 테스트', () => {
   it('비슷한 영상 추천 API', async () => {
-    const { queryByText, findByText } = render(<LearnDetail />, { wrapper: createWrapper() });
-    const SIMILAR_NEWS_TITLE = "우크라이나 재건회의 '루가노 선언' 채택";
+    const { queryByText, findByText } = setup();
 
     expect(queryByText(SIMILAR_NEWS_TITLE)).toBeNull();
     expect(await findByText(SIMILAR_NEWS_TITLE)).toBeInTheDocument();
   });
 
   it('뉴스 상세 정보 API (인증)', async () => {
-    const { queryByText, findByText } = render(<LearnDetail />, { wrapper: createWrapper() });
-    const NEWS_TITLE = '북, 최근 임진강 상류 황강댐 수문 개방';
+    const { queryByText, findByText } = setup();
 
     expect(queryByText(NEWS_TITLE)).toBeNull();
     expect(await findByText(NEWS_TITLE)).toBeInTheDocument();
   });
 });
 
-describe('메모 통합 테스트', () => {
-  it('메모 추가 테스트', async () => {
-    const { getByText, getByRole, queryByText, queryByRole, findByText, findByRole } = render(<LearnDetail />, {
-      wrapper: createWrapper(),
-    });
+const MEMO_FORM = '메모 입력창';
+const SUBMIT = '완료';
+const CANCEL = '취소';
 
-    const NEWS_TITLE = '북, 최근 임진강 상류 황강댐 수문 개방';
+describe('메모 추가 통합 테스트', () => {
+  const ADD_MEMO_BUTTON = '메모 추가';
+  const MEMO_KEYWORD = '군남댐';
+  const MEMO_CONTENT = '발음 주의';
+
+  it('메모 추가 테스트', async () => {
+    const { getByText, getByRole, queryByText, findByText, findByRole } = setup();
+
     expect(await findByText(NEWS_TITLE)).toBeInTheDocument();
 
-    const highlight = getByText('군남댐');
+    const highlight = getByText(MEMO_KEYWORD);
     await userEvent.pointer([{ target: highlight }, { keys: '[MouseRight]', target: highlight }]);
-    expect(await findByText('메모 추가')).toBeInTheDocument();
+    expect(await findByText(ADD_MEMO_BUTTON)).toBeInTheDocument();
 
-    const addMemoButton = getByText('메모 추가');
+    const addMemoButton = getByText(ADD_MEMO_BUTTON);
     await userEvent.click(addMemoButton);
-    expect(await findByRole('heading', { name: '군남댐' })).toBeInTheDocument();
+    expect(await findByRole('heading', { name: MEMO_KEYWORD })).toBeInTheDocument();
 
-    const memoTextarea = getByRole('textbox', { name: '메모 입력창' });
-    const submitButton = getByRole('button', { name: '완료' });
+    const memoTextarea = getByRole('textbox', { name: MEMO_FORM });
+    const submitButton = getByRole('button', { name: SUBMIT });
+
     expect(submitButton).toBeDisabled();
-
-    await userEvent.type(memoTextarea, '발음 주의');
+    await userEvent.type(memoTextarea, MEMO_CONTENT);
     expect(submitButton).toBeEnabled();
+
     await userEvent.click(submitButton);
-    expect(queryByRole('textbox', { name: '메모 입력창' })).toBeNull();
-    expect(queryByText('발음 주의')).toBeInTheDocument();
+    expect(queryByText(MEMO_CONTENT)).toBeInTheDocument();
   });
+});
+
+describe('메모 수정 통합 테스트', () => {
+  const EDIT_MEMO_BUTTON = '메모 수정';
+  const MEMO_KEYWORD = '호우로';
+  const OLD_MEMO_CONTENT = '숨을 크게 들이마시고';
+  const NEW_MEMO_CONTENT = '숨을 작게 들이마시고';
 
   it('메모 수정 테스트', async () => {
-    const { getByText, getAllByText, getByRole, queryByText, queryByRole, findByText, findByRole } = render(
-      <LearnDetail />,
-      {
-        wrapper: createWrapper(),
-      },
-    );
+    const { getByText, getAllByText, getByRole, queryByText, findByText, findByRole } = setup();
 
-    const NEWS_TITLE = '북, 최근 임진강 상류 황강댐 수문 개방';
     expect(await findByText(NEWS_TITLE)).toBeInTheDocument();
-    expect(getByText('숨을 크게 들이마시고')).toBeInTheDocument();
+    expect(getByText(OLD_MEMO_CONTENT)).toBeInTheDocument();
 
-    const highlight = getAllByText('호우로')[0];
+    const highlight = getAllByText(MEMO_KEYWORD)[0];
     await userEvent.pointer([{ target: highlight }, { keys: '[MouseRight]', target: highlight }]);
-    expect(await findByText('메모 수정')).toBeInTheDocument();
+    expect(await findByText(EDIT_MEMO_BUTTON)).toBeInTheDocument();
 
-    const addMemoButton = getByText('메모 수정');
+    const addMemoButton = getByText(EDIT_MEMO_BUTTON);
     await userEvent.click(addMemoButton);
-    expect(await findByRole('heading', { name: '호우로' })).toBeInTheDocument();
+    expect(await findByRole('heading', { name: MEMO_KEYWORD })).toBeInTheDocument();
 
-    const memoTextarea = getByRole('textbox', { name: '메모 입력창' });
-    const submitButton = getByRole('button', { name: '완료' });
+    const memoTextarea = getByRole('textbox', { name: MEMO_FORM });
+    const submitButton = getByRole('button', { name: SUBMIT });
 
-    await userEvent.type(memoTextarea, '숨을 작게 들이마시고');
+    await userEvent.type(memoTextarea, NEW_MEMO_CONTENT);
+
     await userEvent.click(submitButton);
-    expect(queryByRole('textbox', { name: '메모 입력창' })).toBeNull();
-    expect(queryByText('숨을 작게 들이마시고')).toBeInTheDocument();
+    expect(queryByText(NEW_MEMO_CONTENT)).toBeInTheDocument();
   });
 });
