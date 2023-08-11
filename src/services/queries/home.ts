d0606c17f9b6c8a73b51845cf3c1037a633bea76
solar3070 <>
2023-08-11 16:14:59 +0900

    refactor: 홈 페이지 SSG

--- src/services/queries/home.ts
+++ src/services/queries/home.ts
@@ -1,15 +1,18 @@
 import { api } from '@src/services/api';
+import { VideoData } from '@src/types/home/remote';
 import { useQuery } from '@tanstack/react-query';
 
-export const useGetRecommendVideoList = () => {
+export const useGetRecommendVideoList = (initialRecommendNewsList: VideoData[]) => {
   return useQuery(['getRecommendVideoList'], () => api.homeService.getVideoData(), {
+    initialData: initialRecommendNewsList,
     cacheTime: Infinity,
     staleTime: Infinity,
   });
 };
 
-export const useGetSpeechGuideList = () => {
+export const useGetSpeechGuideList = (initialSpeechGuideList: VideoData[]) => {
   return useQuery(['getSpeechGuideList'], () => api.homeService.getSpeechGuideData(), {
+    initialData: initialSpeechGuideList,
     cacheTime: Infinity,
     staleTime: Infinity,
   });
